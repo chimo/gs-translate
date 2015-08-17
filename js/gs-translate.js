ec4721b079deaf04c444236de41d5c7208a22291
Chimo <chimo@chromic.org>
2015-08-16 23:20:30 -0400

    Formatting

--- js/gs-translate.js
+++ js/gs-translate.js
@@ -1,49 +1,50 @@
-(function (window, $) {
-    'use strict';
+/*global _peopletagAC: false*/
+( function( window, $ ) {
+    "use strict";
 
     var gsTranslate = window.gsTranslate,
         encode = encodeURIComponent,
         translate,
-        // https://gitorious.org/social/mainline/source/d318b5c10e557c447fc341747f72bf9cd73a6246:lib/action.php#L430
-        installdir = _peopletagAC.replace("/main/peopletagautocomplete", "");
+        installdir = _peopletagAC.replace( "/main/peopletagautocomplete", "" ),
+        $dialog = $( "<div class='gs-translate-dialog'>" ).dialog( { autoOpen: false } )/*.appendTo( "body" )*/;
 
-    translate = function (text) {
-        var appId        = 'Bearer ',    // FIXME legacy: http://msdn.microsoft.com/en-us/library/hh454950.aspx
-            to           = gsTranslate.targetLanguage || 'en',
-            callbackName = 'gsTranslate.callback',
+    translate = function( text ) {
+        var appId        = "Bearer ",    /* FIXME legacy: http://msdn.microsoft.com/en-us/library/hh454950.aspx */
+            to           = gsTranslate.targetLanguage || "en",
+            callbackName = "gsTranslate.callback",
             script;
 
-        script = document.createElement('script');
+        script = document.createElement( "script" );
 
-        script.src = '//api.microsofttranslator.com/V2/Ajax.svc/Translate' +
-            '?appId=' + encode(appId) + encode(gsTranslate.accessToken.token) +
-            '&to=' + encode(to) +
-            '&text=' + encode(text) +
-            '&oncomplete=' + encode(callbackName);
+        script.src = "//api.microsofttranslator.com/V2/Ajax.svc/Translate" +
+            "?appId=" + encode( appId ) + encode( gsTranslate.accessToken.token ) +
+            "&to=" + encode( to ) +
+            "&text=" + encode( text ) +
+            "&oncomplete=" + encode( callbackName );
 
-        document.body.appendChild(script);
+        document.body.appendChild( script );
     };
 
-    $(document).on('click', '.gs-translate', function (e) {
+    $( document ).on( "click", ".gs-translate", function( e ) {
         e.preventDefault();
 
-        var $this = $(this),
-            text = $this.closest('.notice').find('.e-content').first().text();
+        var $this = $( this ),
+            text = $this.closest( ".notice" ).find( ".e-content" ).first().text();
 
-        if (gsTranslate.accessToken.expires < Date.now()) {
-            // TODO: Error handling
-            $.getJSON(installdir + '/main/translatenotice/renewtoken', function (data) {
+        if ( gsTranslate.accessToken.expires < Date.now() ) {
+            /* TODO: Error handling */
+            $.getJSON( installdir + "/main/translatenotice/renewtoken", function( data ) {
                 gsTranslate.accessToken.token = data.token;
                 gsTranslate.accessToken.expires = data.expires;
 
-                translate(text);
-            });
+                translate( text );
+            } );
         } else {
-            translate(text);
+            translate( text );
         }
-    });
+    } );
 
-    gsTranslate.callback = function (response) {
-        alert(response);
+    gsTranslate.callback = function( response ) {
+        $dialog.text( response ).dialog( "open" );
     };
-}(window, jQuery));
+}( window, jQuery ) );
